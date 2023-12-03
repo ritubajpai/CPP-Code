6b5e5228c08ee8eece9a2ee9498113f4774b3c68
Ritu Bajpai <57282688+ritubajpai@users.noreply.github.com>
2023-12-03 21:54:30 +0530

    OnesComplementOnesCommit

---
+++ OnesComplement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+ 
+void onescomp(int num)
+{
+    int rem;
+ 
+    if (num <= 1)
+    {
+        cout << !num;
+        return;
+    }
+    rem = num % 2;
+    onescomp(num / 2);
+    cout << !rem;
+}
+ 
+int main()
+{
+    int dec, bin;
+    cout << "Enter the number : ";
+    cin >> dec;
+ 
+    if (dec < 0)
+        cout << dec << " is not a positive integer." << endl;
+    else
+    {
+        cout << "The ones complement form of " << dec << " is ";
+        onescomp(dec);
+        cout << endl;
+    }
+    return 0;
+}
