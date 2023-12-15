6a8e3725c208a834738817b85c9ede45a4296f2e
Ritu Bajpai <57282688+ritubajpai@users.noreply.github.com>
2023-12-15 23:57:43 +0530

    BitwiseOperationsCommit

---
+++ BitwiseOperations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a = 7; // a = 111
+    int b = 5; // b = 101
+ 
+    cout << "Bitwise Operators\n";
+    cout << "a & b = " << (a&b) << "\n";
+    cout << "a | b = " << (a|b) << "\n";
+    cout << "a ^ b = " << (a^b) << "\n";
+    cout << "~a = " << (~a) << "\n";
+    cout << "~b = " << (~b) << "\n";
+    cout << "a >> b = " << (a>>b) << "\n";
+    cout << "a << b = " << (a<<b) << "\n";
+}
